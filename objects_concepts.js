23ebb723cc7cc5d94af8b59cc1fbad1b3faec4c4
Martin T <mtitus575@gmail.com>
2025-02-03 09:47:30 +0000

    JS Object Concepts - Study Notes

---
+++ objects_concepts.js
@@ -0,0 +1,23 @@
+/*
+Intro:
+- Objects can be assigned to VARIABLES just like any other data type.
+
+1. What are objects?
+- Objects are the 8th data type in JS.
+- The curly braces, {}, is used to define an Object Literal (the code block of an object)
+- It holds UNORDERED data.
+- The data is stored in key-value pairs. 
+
+2. Object structure/syntax.
+
+3. Keys explained.
+- A key is similar to a variable name that points to a location in memory where the value is held.
+- Key are STRINGS by default (the system stores it as a string in the background)
+    -You don't need to use the quotation marks for single worded keys.
+    -When defining a key with a space, you MUST use quotes.
+
+4. Values explained.
+- Values can be any data type.
+
+5. 
+*/
